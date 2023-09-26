faa37092a876bf76aa55606f019b3255e8450050
Or Hasson <hassonor@gmail.com>
2023-09-26 16:15:38 +0300

    add more numpy examples

--- lessons/00_introduction/numpy_arrays.py
+++ lessons/00_introduction/numpy_arrays.py
@@ -35,3 +35,55 @@ print(a.min())  # 2
 # Index of max and min values
 print(a.argmax())  # 6
 print(a.argmin())  # 1
+
+# Range
+
+c = np.arange(0, 10)
+print(c)  # [0,1,2,3,4,5,6,7,8,9]
+
+d = np.arange(0, 11, 2)
+print(d)  # [0,2,4,6,8,10]
+
+# Linspace
+
+x1 = np.linspace(0, 20, 5)  # 3rd arg is the count of the numbers to generate
+print(x1)  # [0., 7., 10.,15.,20.]
+
+x2 = np.linspace(0, 10, 3)
+print(x2)  # [0., 5., 10.]
+
+x3 = np.linspace(0, 10, 50)  # Will generate 50 numbers between 0 and 10
+print(x3)
+print(x3.shape)  # (50,)
+
+# Reshape
+y = np.arange(0, 25)
+print(y)  # [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+print(y.shape)  # (25,)
+print(y.reshape(5, 5))  # [[0,1,2,3,4],[5,6,7,8,9],[10,11,12,13,14],[15,16,17,18,19],[20,21,22,23,24]]
+print(y.reshape(1, 25))  # [[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]]
+print(y.reshape(25,
+                1))  # [[0],[1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12],[13],[14],[15],[16],[17],[18],[19],[20],[21],[22],[23],[24]]]
+
+# Zeroes and ones
+
+print(np.zeros(3))  # [0.,0.,0.]
+print(np.zeros(shape=(3, 3)))  # [[0.,0.,0.],[0.,0.,0.],[0.,0.,0.]]
+print(np.zeros((3, 3)))  # [[0.,0.,0.],[0.,0.,0.],[0.,0.,0.]]
+
+print(np.ones(5))  # [1.,1.,1.,1.,1.]
+print(np.ones((4, 3)))  # [[1.,1.,1.],[1.,1.,1.],[1.,1.,1.],[1.,1.,1.]]
+print(7 * np.ones((3, 3)))  # [[7.,7.,7.],[7.,7.,7.],[7.,7.,7.],[7.,7.,7.]]
+
+# Eye, generating Identity Matrix
+print(np.eye(3))  # [[1.,0.,0.],[0.,1.,0.],[0.,0.,1.]]
+
+# Random Numbers: rand, randint
+x1 = np.random.randn(3, 3)  # will generate random numbers from normal distribution
+print(x1)
+
+x2 = np.random.rand(3, 3)  # will generate random numbers between 0 and 1
+print(x2)
+
+x3 = np.random.randint(3, 8, 10)  # will generate 10 random integers between 3 and 8
+print(x3)
